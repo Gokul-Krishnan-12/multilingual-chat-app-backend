382c30bdee253983ccad48eb19ec5a28eeddd3df
Gokul Krishnan <gokulkrish80@gmail.com>
2026-01-03 12:13:20 +0530

    feat : update url

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: process.env.FRONTEND_URL,
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
   })
@@ -30,7 +30,7 @@ const server = http.createServer(app);
 
 export const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: process.env.FRONTEND_URL,
     credentials: true,
   },
 });
